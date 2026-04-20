f7a1558e7957205c7dea3debc34443d164884aca
Li Yi <denverdino@gmail.com>
2026-04-20 13:50:09 +0800

    Add the Alibaba Cloud Function Compute Backend for Deep Agents Sandbox

---
+++ libs/partners/alicloud-fc/langchain_alicloud_fc/_version.py
@@ -0,0 +1,3 @@
+"""Version information for `langchain-alicloud-fc`."""
+
+__version__ = "0.0.1"  # x-release-please-version
---
+++ libs/partners/alicloud-fc/tests/test_import.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import langchain_alicloud_fc
+
+
+def test_import_alicloud_fc() -> None:
+    assert langchain_alicloud_fc is not None
